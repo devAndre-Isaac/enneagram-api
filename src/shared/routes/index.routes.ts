e28798929bd9a26bdd8b859dcc5c1e641ef06c20
André Isaac <andreisaac344@gmail.com>
2023-05-28 17:53:30 -0300

    feat: fix a route imports

--- src/shared/routes/index.routes.ts
+++ src/shared/routes/index.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import questionsRouter from 'src/modules/Questions/routes/questions.routes';
+import questionsRouter from '../../modules/Questions/routes/questions.routes';
 import clientsRouter from '../../modules/Clients/routes/clients.routes';
 import usersRouter from '../../modules/Users/routes/users.routes';
 
